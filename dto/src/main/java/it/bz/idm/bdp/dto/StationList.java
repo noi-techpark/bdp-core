61d10350efadf6e1a2a66b2171b6e9aff9ec9b0a
bertolla <patrick.bertolla@idm-suedtirol.com>
2017-12-07 16:24:01 +0100

    fix merge conflicts

---
+++ dto/src/main/java/it/bz/idm/bdp/dto/StationList.java
@@ -0,0 +1,14 @@
+package it.bz.idm.bdp.dto;
+
+import java.util.ArrayList;
+
+import it.bz.idm.bdp.dto.StationDto;
+
+public class StationList extends ArrayList<StationDto>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2408060694809964354L;
+
+}
