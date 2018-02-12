d876fb2989953125eb458d16660121f6ba62f7cb
bertolla <patrick.bertolla@idm-suedtirol.com>
2018-02-12 14:54:11 +0100

    hotfix for forecasts

--- dto/src/main/java/it/bz/idm/bdp/dto/bluetooth/BluetoothRecordExtendedDto.java
+++ dto/src/main/java/it/bz/idm/bdp/dto/bluetooth/BluetoothRecordExtendedDto.java
@@ -1,31 +1,27 @@
 package it.bz.idm.bdp.dto.bluetooth;
 
-import java.util.Date;
-
-import it.bz.idm.bdp.dto.bluetooth.BluetoothRecordDto;
-
 public class BluetoothRecordExtendedDto extends BluetoothRecordDto{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -3113420019159385769L;
-	private Date created_on;
+	private Long created_on;
 
 
 
 	public BluetoothRecordExtendedDto() {
 	}
 
-	public BluetoothRecordExtendedDto(Long timestamp, Double value,Date created_on) {
+	public BluetoothRecordExtendedDto(Long timestamp, Double value,Long created_on) {
 		super(timestamp, value);
 		this.created_on=created_on;
 	}
 
-	public Date getCreated_on() {
+	public Long getCreated_on() {
 		return created_on;
 	}
 
-	public void setCreated_on(Date created_on) {
+	public void setCreated_on(Long created_on) {
 		this.created_on = created_on;
 	}
 
