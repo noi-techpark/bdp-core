99dfcc2fa79c84fb73561768d3c190955f4eafb0
bertolla <patrick.bertolla@idm-suedtirol.com>
2018-04-17 16:49:43 +0200

    authentication + test

---
+++ dal/src/main/java/it/bz/idm/bdp/dal/authentication/BDPPermission.java
@@ -0,0 +1,58 @@
+package it.bz.idm.bdp.dal.authentication;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+import it.bz.idm.bdp.dal.DataType;
+import it.bz.idm.bdp.dal.Station;
+
+@Entity
+public class BDPPermission {
+    @Id
+    @GeneratedValue(generator="permission_gen",strategy=GenerationType.SEQUENCE)
+    @SequenceGenerator(name="permission_gen", sequenceName = "permissions_seq",schema="intime",allocationSize=1)
+    private Long id;
+    
+    @ManyToOne
+	private BDPRole role;
+    @ManyToOne
+	private Station station;
+    @ManyToOne
+	private DataType type;
+	private Integer period;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public BDPRole getRole() {
+		return role;
+	}
+	public void setRole(BDPRole role) {
+		this.role = role;
+	}
+	public Station getStation() {
+		return station;
+	}
+	public void setStation(Station station) {
+		this.station = station;
+	}
+	public DataType getType() {
+		return type;
+	}
+	public void setType(DataType type) {
+		this.type = type;
+	}
+	public Integer getPeriod() {
+		return period;
+	}
+	public void setPeriod(Integer period) {
+		this.period = period;
+	}
+	
+}
--- writer/src/test/java/it/bz/idm/bdp/SecurityIT.java
+++ writer/src/test/java/it/bz/idm/bdp/SecurityIT.java
@@ -1,24 +1,57 @@
 package it.bz.idm.bdp;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
+import org.junit.AfterClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import it.bz.idm.bdp.dal.DataType;
+import it.bz.idm.bdp.dal.Measurement;
+import it.bz.idm.bdp.dal.Station;
+import it.bz.idm.bdp.dal.authentication.BDPPermission;
 import it.bz.idm.bdp.dal.authentication.BDPRole;
+import it.bz.idm.bdp.dal.environment.Environmentstation;
 import it.bz.idm.bdp.dal.util.JPAUtil;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"/META-INF/spring/applicationContext.xml"})
 public class SecurityIT extends AbstractJUnit4SpringContextTests{
-	public EntityManager entityManager = JPAUtil.createEntityManager();
+	private static EntityManager entityManager;
+	private BDPRole role,role2;
+	private Station station;
+	private DataType type;
+	private BDPPermission permission;
 	
+	@PostConstruct
+	public void setup() {
+		entityManager = JPAUtil.createEntityManager();
+		entityManager.getTransaction().begin();
+		role = new BDPRole();
+		role.setName("Holla");
+		role.setDescription("The Parent Role");
+		role2 = new BDPRole();
+		role2.setName("Halla");
+		role2.setDescription("The Child Role");
+		role2.setParent(role);
+		station= new Environmentstation("BLuesky");
+		type = new DataType("NO2","mg","Fake type","Instants");
+		permission = new BDPPermission();
+		permission.setPeriod(500);
+		permission.setRole(role);
+		permission.setStation(station);
+		permission.setType(type);
+
+	}
 	
 	@Test
 	public void testJPAUtilInit() {
@@ -26,8 +59,23 @@ public class SecurityIT extends AbstractJUnit4SpringContextTests{
 	}
 
 	@Test
-	public void testRoleTable() {
-		BDPRole role = BDPRole.findByName(entityManager, "role1");
-		assertNull(role);
+	public void testGetRulesForRole() {
+		BDPRole r = BDPRole.findByName(entityManager, role.getName());
+		TypedQuery<Measurement> query = entityManager.createQuery(
+				"select m from Measurement m join BDPRules ru where ru.role = :role and (m.station = ru.station or ru.station = null)"
+				+ " and (m.type = ru.type or ru.type = null) and (m.period = ru.period or ru.period = null)",
+				Measurement.class);
+		query.setParameter("role", r);
+		List<Measurement> resultList = query.getResultList();
+		assertNotNull(resultList);
+		
+	}
+	
+	@Test
+	public void testInsertPermission() {
+	}
+	
+	@AfterClass
+	public static void cleanup() {
 	}
 }
