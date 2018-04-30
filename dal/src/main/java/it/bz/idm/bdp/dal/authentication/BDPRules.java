a370e78329b75617eab5e7372aee6d8b5fdfe80a
Peter Moser <peter.moser@idm-suedtirol.com>
2018-04-30 14:01:10 +0200

    Switch rules <-> permissions

--- dal/src/main/java/it/bz/idm/bdp/dal/authentication/BDPRules.java
+++ dal/src/main/java/it/bz/idm/bdp/dal/authentication/BDPRules.java
@@ -1,29 +1,35 @@
 package it.bz.idm.bdp.dal.authentication;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-
-import org.hibernate.annotations.Immutable;
+import javax.persistence.SequenceGenerator;
 
 import it.bz.idm.bdp.dal.DataType;
 import it.bz.idm.bdp.dal.Station;
 
-@Immutable
 @Entity
 public class BDPRules {
-	
-	@Id
-	private Long uuid;
+    @Id
+	@GeneratedValue(generator = "rules_gen", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "rules_gen", sequenceName = "rules_seq", schema = "intime", allocationSize = 1)
+    private Long id;
 
-	@ManyToOne
+    @ManyToOne
 	private BDPRole role;
-	@ManyToOne
+    @ManyToOne
 	private Station station;
-	@ManyToOne
+    @ManyToOne
 	private DataType type;
-	
 	private Integer period;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public BDPRole getRole() {
 		return role;
 	}
@@ -48,4 +54,5 @@ public class BDPRules {
 	public void setPeriod(Integer period) {
 		this.period = period;
 	}
+
 }
--- writer/src/test/java/it/bz/idm/bdp/SecurityIT.java
+++ writer/src/test/java/it/bz/idm/bdp/SecurityIT.java
@@ -18,8 +18,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import it.bz.idm.bdp.dal.DataType;
 import it.bz.idm.bdp.dal.Measurement;
 import it.bz.idm.bdp.dal.Station;
-import it.bz.idm.bdp.dal.authentication.BDPPermission;
 import it.bz.idm.bdp.dal.authentication.BDPRole;
+import it.bz.idm.bdp.dal.authentication.BDPRules;
 import it.bz.idm.bdp.dal.environment.Environmentstation;
 import it.bz.idm.bdp.dal.util.JPAUtil;
 
@@ -30,8 +30,8 @@ public class SecurityIT extends AbstractJUnit4SpringContextTests{
 	private BDPRole role,role2;
 	private Station station;
 	private DataType type;
-	private BDPPermission permission;
-	
+	private BDPRules rule;
+
 	@PostConstruct
 	public void setup() {
 		entityManager = JPAUtil.createEntityManager();
@@ -45,14 +45,13 @@ public class SecurityIT extends AbstractJUnit4SpringContextTests{
 		role2.setParent(role);
 		station= new Environmentstation("BLuesky");
 		type = new DataType("NO2","mg","Fake type","Instants");
-		permission = new BDPPermission();
-		permission.setPeriod(500);
-		permission.setRole(role);
-		permission.setStation(station);
-		permission.setType(type);
-
+		rule = new BDPRules();
+		rule.setPeriod(500);
+		rule.setRole(role);
+		rule.setStation(station);
+		rule.setType(type);
 	}
-	
+
 	@Test
 	public void testJPAUtilInit() {
 		assertNotNull(entityManager);
@@ -64,15 +63,15 @@ public class SecurityIT extends AbstractJUnit4SpringContextTests{
 		//JOIN between unrelated entities https://www.thoughts-on-java.org/how-to-join-unrelated-entities/
 		//had to upgrade hibernate to resolve this known issue https://hibernate.atlassian.net/browse/HHH-2772
 		TypedQuery<Measurement> query = entityManager.createQuery(
-				"select m from Measurement m join BDPRules ru on (m.station = ru.station or ru.station = null)"
+				"select m from Measurement m join BDPPermissions ru on (m.station = ru.station or ru.station = null)"
 				+ " and (m.type = ru.type or ru.type = null) and (m.period = ru.period or ru.period = null) where ru.role = :role",
 				Measurement.class);
 		query.setParameter("role", r);
 		List<Measurement> resultList = query.getResultList();
 		assertNotNull(resultList);
-		
+
 	}
-	
+
 	@Test
 	public void testRulesForLastRecord() {
 		BDPRole r = BDPRole.findByName(entityManager, role.getName());
@@ -80,9 +79,9 @@ public class SecurityIT extends AbstractJUnit4SpringContextTests{
 		DataType type = DataType.findByCname(entityManager, this.type.getCname());
 		Measurement m = Measurement.findLatestEntry(entityManager, station, null, null, r);
 		assertNotNull(m);
-		
+
 	}
-	
+
 	@AfterClass
 	public static void cleanup() {
 	}
