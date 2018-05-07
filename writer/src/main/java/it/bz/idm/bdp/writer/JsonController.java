4a62f80712e5005a691cfd977e8bce4fb8e21078
Peter Moser <peter.moser@idm-suedtirol.com>
2018-05-07 16:09:35 +0200

    Test: syncStations and syncDataTypes + JPAUtil.getInstanceByType  better error handling

--- writer/src/main/java/it/bz/idm/bdp/writer/JsonController.java
+++ writer/src/main/java/it/bz/idm/bdp/writer/JsonController.java
@@ -19,22 +19,25 @@ import it.bz.idm.bdp.dto.StationDto;
 @RequestMapping("/json")
 @Controller
 public class JsonController extends DataManager{
+	@Override
 	@RequestMapping(value = "/getDateOfLastRecord/{integreenTypology}/", method = RequestMethod.GET)
 	public @ResponseBody Date getDateOfLastRecord(@PathVariable("integreenTypology") String stationType,@RequestParam("stationId") String stationId,
 			@RequestParam(value="typeId") String typeId, @RequestParam(value="period",required=false) Integer period) {
 		return (Date) super.getDateOfLastRecord(stationType, stationId, typeId, period);
 	}
-	
+
+	@Override
 	@RequestMapping(value = "/stationsWithoutMunicipality", method = RequestMethod.GET)
 	public @ResponseBody List<StationDto> getStationsWithoutMunicipality() {
 		return super.getStationsWithoutMunicipality();
 	}
-	
+
 	@RequestMapping(value = "/pushRecords/{integreenTypology}", method = RequestMethod.POST)
 	public @ResponseBody Object pushRecords(@RequestBody(required = true) DataMapDto<RecordDtoImpl> stationData,
 			@PathVariable String integreenTypology) {
 		return super.pushRecords(integreenTypology, stationData);
 	}
+	@Override
 	@RequestMapping(value = "/patchStations", method = RequestMethod.POST)
 	public @ResponseBody void patchStations(@RequestBody(required = true) List<StationDto> stations) {
 		super.patchStations(stations);
@@ -45,8 +48,9 @@ public class JsonController extends DataManager{
 		return super.syncStations(integreenTypology, data);
 	}
 
+	@Override
 	@RequestMapping(value = "/syncDataTypes", method = RequestMethod.POST)
 	public @ResponseBody Object syncDataTypes(@RequestBody(required = true) List<DataTypeDto> data) {
-		return super.syncDataTypes(null, data);
+		return super.syncDataTypes(data);
 	}
 }
