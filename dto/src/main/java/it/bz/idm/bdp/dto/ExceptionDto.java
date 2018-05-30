26a1ec5765974b5b3796cc802a672274d4bb4c07
bertolla <patrick.bertolla@idm-suedtirol.com>
2018-05-30 16:18:48 +0200

    added errorDto

--- dto/src/main/java/it/bz/idm/bdp/dto/ExceptionDto.java
+++ dto/src/main/java/it/bz/idm/bdp/dto/ExceptionDto.java
@@ -8,18 +8,25 @@ public class ExceptionDto implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -2644337056228195945L;
+	private Integer status;
 	private String name;
-	private String stackTrace;
+	private String description;
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getStackTrace() {
-		return stackTrace;
+	public Integer getStatus() {
+		return status;
 	}
-	public void setStackTrace(String stackTrace) {
-		this.stackTrace = stackTrace;
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
 	}
 }
