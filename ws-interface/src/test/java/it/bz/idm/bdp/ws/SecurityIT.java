906ac4564ba732b2068dfbc712389412fa8b3ed8
bertolla <patrick.bertolla@idm-suedtirol.com>
2018-05-30 16:36:10 +0200

    added integration test for auth token

--- ws-interface/src/test/java/it/bz/idm/bdp/ws/SecurityIT.java
+++ ws-interface/src/test/java/it/bz/idm/bdp/ws/SecurityIT.java
@@ -6,31 +6,37 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Date;
 
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import it.bz.idm.bdp.dto.security.AccessTokenDto;
 import it.bz.idm.bdp.dto.security.JwtTokenDto;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/META-INF/spring/applicationContext*.xml"})
 public class SecurityIT {
 	
 	private static final String TOKEN_PREFIX = "Bearer ";
 	private static final String BAD_REFRESH_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwYXRyaWSrLdJlcnRvbGxhIiwicm9sZXMiOiJST0xFX0hhbGxhIn4.j63Q3dNTNbe9jDV-Y0ZaJYglp1u2mNf26r1Zh9wvHwaXoXlC1Cav_LOxaHDA-79az7osM6cYhC4NMjdro5kP1A";
 	
-	@Value("testusername")
 	private String username;
 	
-	@Value("testpassword")
 	private String password;
 	
+	private Configuration config;
 
+
+	public SecurityIT() {
+		try {
+			config = new PropertiesConfiguration("application.properties");
+			this.username = config.getString("test.username");
+			this.password = config.getString("test.password");
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
+	}
 	public RestClient client = new RestClient() {
 		
 		@Override
@@ -44,6 +50,11 @@ public class SecurityIT {
 		JwtTokenDto authenticationToken = client.fetchRefreshToken(username,password);
 		assertNotNull(authenticationToken);
 		assertTrue(authenticationToken.getRefreshToken()!= null && !authenticationToken.getRefreshToken().isEmpty());
+		try {
+			new PropertiesConfiguration("application.properties");
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
