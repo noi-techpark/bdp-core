c97c5bc827675b63e2abcda2a169cdf4b563539c
bertolla <patrick.bertolla@idm-suedtirol.com>
2018-06-12 10:21:18 +0200

    upgrade code against deprecation

--- ws-interface/src/main/java/it/bz/idm/bdp/ws/util/SwaggerWebAdapter.java
+++ ws-interface/src/main/java/it/bz/idm/bdp/ws/util/SwaggerWebAdapter.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class SwaggerWebAdapter extends WebMvcConfigurerAdapter{
+public class SwaggerWebAdapter implements WebMvcConfigurer{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		  registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/");
