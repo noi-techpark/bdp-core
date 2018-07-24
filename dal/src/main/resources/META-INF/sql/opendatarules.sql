e6e6963e4749c112e488edb250b6413bfb34b9e8
bertolla <patrick.bertolla@idm-suedtirol.com>
2018-07-24 14:02:59 +0200

    improve opendata rules

--- dal/src/main/resources/META-INF/sql/opendatarules.sql
+++ dal/src/main/resources/META-INF/sql/opendatarules.sql
@@ -1,1 +1,5 @@
+begin;
+delete from bdprules ru  where ru.role_id in (select id from bdprole where name='GUEST');
 insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'GUEST' and s.stationtype not in ('TrafficSensor','Environmentstation');
+refresh materialized view bdppermissions;
+commit;
