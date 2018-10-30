b6cb4b24c4082138613ae2bde0a73b04279b6965
Alex Lanz <alex@krumer.it>
2018-10-30 16:40:19 +0100

    Move tmp folder into docker folder

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p ~/.m2
-echo "<settings><localRepository>$PWD/tmp/.m2</localRepository></settings>" > ~/.m2/settings.xml
+echo "<settings><localRepository>$PWD/docker/tmp/.m2</localRepository></settings>" > ~/.m2/settings.xml
 
 /bin/bash -c "$@"
