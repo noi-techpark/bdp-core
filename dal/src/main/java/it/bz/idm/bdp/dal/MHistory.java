90b232696977962fd02ecaf463635117fe6e24e3
Peter Moser <peter.moser@idm-suedtirol.com>
2018-11-23 11:23:45 +0100

    dal: Check for correct DataMapDto on push

--- dal/src/main/java/it/bz/idm/bdp/dal/MHistory.java
+++ dal/src/main/java/it/bz/idm/bdp/dal/MHistory.java
@@ -32,6 +32,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
 
 import it.bz.idm.bdp.dal.authentication.BDPRole;
+import it.bz.idm.bdp.dal.util.JPAException;
 import it.bz.idm.bdp.dto.DataMapDto;
 import it.bz.idm.bdp.dto.RecordDto;
 import it.bz.idm.bdp.dto.RecordDtoImpl;
@@ -102,118 +103,120 @@ public abstract class MHistory {
 
 	public static void pushRecords(EntityManager em, String stationType, Object... objects) {
 		Object object = objects[0];
+		if (! (object instanceof DataMapDto)) {
+			throw new JPAException("Given data is not a DataMapDto");
+		}
+
 		BDPRole adminRole = BDPRole.fetchAdminRole(em);
-		if (object instanceof DataMapDto) {
-			@SuppressWarnings("unchecked")
-			DataMapDto<RecordDtoImpl> dto = (DataMapDto<RecordDtoImpl>) object;
-			try{
-				for (Entry<String, DataMapDto<RecordDtoImpl>> entry:dto.getBranch().entrySet()){
-					Station station = Station.findStation(em, stationType, entry.getKey());
-					if (station == null) {
-						System.out.println("pushRecords: Station '" + stationType + "/" + entry.getKey() + "' not found. Skipping...");
-						continue;
-					}
-					for(Entry<String,DataMapDto<RecordDtoImpl>> typeEntry : entry.getValue().getBranch().entrySet()) {
-						try {
-							DataType type = DataType.findByCname(em, typeEntry.getKey());
-							if (type == null) {
-								System.out.println("pushRecords: Type '" + typeEntry.getKey() + "' not found. Skipping...");
-								continue;
-							}
-
-							List<? extends RecordDtoImpl> dataRecords = typeEntry.getValue().getData();
-							if (dataRecords.isEmpty()) {
-								System.out.println("pushRecords: Empty data set. Skipping...");
-								continue;
-							}
-
-							em.getTransaction().begin();
-
-							M latestNumberMeasurement = new Measurement().findLatestEntry(em, station, type, null, adminRole);
-							long latestNumberMeasurementTime = (latestNumberMeasurement != null) ? latestNumberMeasurement.getTimestamp().getTime() : 0;
-							M latestStringMeasurement = new MeasurementString().findLatestEntry(em, station, type, null, adminRole);
-							long latestStringMeasurementTime = (latestStringMeasurement != null) ? latestStringMeasurement.getTimestamp().getTime() : 0;
-
-							Date created_on = new Date();
-							SimpleRecordDto newestStringDto = null;
-							SimpleRecordDto newestNumberDto = null;
-							for (RecordDto recordDto : dataRecords) {
-
-								/*
-								 * XXX We support only SimpleRecordDtos at the moment. This should be removed,
-								 * when we see that we do not need anything else then SimpleRecords
-								 */
-								if (! (recordDto instanceof SimpleRecordDto))
-									continue;
-
-								SimpleRecordDto simpleRecordDto = (SimpleRecordDto)recordDto;
-								Long dateOfMeasurement = simpleRecordDto.getTimestamp();
-								Object valueObj = simpleRecordDto.getValue();
-								if (valueObj instanceof Number) {
-									if (latestNumberMeasurementTime < dateOfMeasurement) {
-										Double value = ((Number)valueObj).doubleValue();
-										MeasurementHistory record = new MeasurementHistory(station, type, value, new Date(dateOfMeasurement), simpleRecordDto.getPeriod(), created_on);
-										em.persist(record);
-									}
-									if (newestNumberDto == null || newestNumberDto.getTimestamp() < simpleRecordDto.getTimestamp()) {
-										newestNumberDto = simpleRecordDto;
-									}
-								} else if (valueObj instanceof String) {
-									if (latestStringMeasurementTime < dateOfMeasurement) {
-										String value = (String) valueObj;
-										MeasurementStringHistory record = new MeasurementStringHistory(station, type, value, new Date(dateOfMeasurement), simpleRecordDto.getPeriod(), created_on);
-										em.persist(record);
-									}
-									if (newestStringDto == null || newestStringDto.getTimestamp() < simpleRecordDto.getTimestamp()) {
-										newestStringDto = simpleRecordDto;
-									}
-								}
-							}
-
-							if (newestNumberDto != null) {
-								Double valueNumber = ((Number)newestNumberDto.getValue()).doubleValue();
-								if (latestNumberMeasurement == null) {
-									latestNumberMeasurement = new Measurement(station, type, valueNumber, new Date(newestNumberDto.getTimestamp()), newestNumberDto.getPeriod());
-									em.persist(latestNumberMeasurement);
-								} else if (newestNumberDto.getTimestamp() > latestNumberMeasurementTime) {
-									latestNumberMeasurement.setTimestamp(new Date(newestNumberDto.getTimestamp()));
-									latestNumberMeasurement.setValue(valueNumber);
-									em.merge(latestNumberMeasurement);
-								}
-							}
-
-							if (newestStringDto != null) {
-								String valueString = (String) newestStringDto.getValue();
-								if (latestStringMeasurement == null) {
-									latestStringMeasurement = new MeasurementString(station, type, valueString, new Date(newestStringDto.getTimestamp()), newestStringDto.getPeriod());
-									em.persist(latestStringMeasurement);
-								} else if (newestStringDto.getTimestamp() > latestStringMeasurementTime) {
-									latestStringMeasurement.setTimestamp(new Date(newestStringDto.getTimestamp()));
-									latestStringMeasurement.setValue(valueString);
-									em.merge(latestStringMeasurement);
-								}
-							}
-
-							em.getTransaction().commit();
-						} catch(Exception ex) {
-							ex.printStackTrace();
-							if (em.getTransaction().isActive())
-								em.getTransaction().rollback();
+		@SuppressWarnings("unchecked")
+		DataMapDto<RecordDtoImpl> dto = (DataMapDto<RecordDtoImpl>) object;
+		try{
+			for (Entry<String, DataMapDto<RecordDtoImpl>> entry:dto.getBranch().entrySet()){
+				Station station = Station.findStation(em, stationType, entry.getKey());
+				if (station == null) {
+					System.out.println("pushRecords: Station '" + stationType + "/" + entry.getKey() + "' not found. Skipping...");
+					continue;
+				}
+				for(Entry<String,DataMapDto<RecordDtoImpl>> typeEntry : entry.getValue().getBranch().entrySet()) {
+					try {
+						DataType type = DataType.findByCname(em, typeEntry.getKey());
+						if (type == null) {
+							System.out.println("pushRecords: Type '" + typeEntry.getKey() + "' not found. Skipping...");
 							continue;
 						}
-					}
 
+						List<? extends RecordDtoImpl> dataRecords = typeEntry.getValue().getData();
+						if (dataRecords.isEmpty()) {
+							System.out.println("pushRecords: Empty data set. Skipping...");
+							continue;
+						}
+
+						em.getTransaction().begin();
+
+						M latestNumberMeasurement = new Measurement().findLatestEntry(em, station, type, null, adminRole);
+						long latestNumberMeasurementTime = (latestNumberMeasurement != null) ? latestNumberMeasurement.getTimestamp().getTime() : 0;
+						M latestStringMeasurement = new MeasurementString().findLatestEntry(em, station, type, null, adminRole);
+						long latestStringMeasurementTime = (latestStringMeasurement != null) ? latestStringMeasurement.getTimestamp().getTime() : 0;
+
+						Date created_on = new Date();
+						SimpleRecordDto newestStringDto = null;
+						SimpleRecordDto newestNumberDto = null;
+						for (RecordDto recordDto : dataRecords) {
+
+							/*
+							 * XXX We support only SimpleRecordDtos at the moment. This should be removed,
+							 * when we see that we do not need anything else then SimpleRecords
+							 */
+							if (! (recordDto instanceof SimpleRecordDto))
+								continue;
+
+							SimpleRecordDto simpleRecordDto = (SimpleRecordDto)recordDto;
+							Long dateOfMeasurement = simpleRecordDto.getTimestamp();
+							Object valueObj = simpleRecordDto.getValue();
+							if (valueObj instanceof Number) {
+								if (latestNumberMeasurementTime < dateOfMeasurement) {
+									Double value = ((Number)valueObj).doubleValue();
+									MeasurementHistory record = new MeasurementHistory(station, type, value, new Date(dateOfMeasurement), simpleRecordDto.getPeriod(), created_on);
+									em.persist(record);
+								}
+								if (newestNumberDto == null || newestNumberDto.getTimestamp() < simpleRecordDto.getTimestamp()) {
+									newestNumberDto = simpleRecordDto;
+								}
+							} else if (valueObj instanceof String) {
+								if (latestStringMeasurementTime < dateOfMeasurement) {
+									String value = (String) valueObj;
+									MeasurementStringHistory record = new MeasurementStringHistory(station, type, value, new Date(dateOfMeasurement), simpleRecordDto.getPeriod(), created_on);
+									em.persist(record);
+								}
+								if (newestStringDto == null || newestStringDto.getTimestamp() < simpleRecordDto.getTimestamp()) {
+									newestStringDto = simpleRecordDto;
+								}
+							}
+						}
+
+						if (newestNumberDto != null) {
+							Double valueNumber = ((Number)newestNumberDto.getValue()).doubleValue();
+							if (latestNumberMeasurement == null) {
+								latestNumberMeasurement = new Measurement(station, type, valueNumber, new Date(newestNumberDto.getTimestamp()), newestNumberDto.getPeriod());
+								em.persist(latestNumberMeasurement);
+							} else if (newestNumberDto.getTimestamp() > latestNumberMeasurementTime) {
+								latestNumberMeasurement.setTimestamp(new Date(newestNumberDto.getTimestamp()));
+								latestNumberMeasurement.setValue(valueNumber);
+								em.merge(latestNumberMeasurement);
+							}
+						}
+
+						if (newestStringDto != null) {
+							String valueString = (String) newestStringDto.getValue();
+							if (latestStringMeasurement == null) {
+								latestStringMeasurement = new MeasurementString(station, type, valueString, new Date(newestStringDto.getTimestamp()), newestStringDto.getPeriod());
+								em.persist(latestStringMeasurement);
+							} else if (newestStringDto.getTimestamp() > latestStringMeasurementTime) {
+								latestStringMeasurement.setTimestamp(new Date(newestStringDto.getTimestamp()));
+								latestStringMeasurement.setValue(valueString);
+								em.merge(latestStringMeasurement);
+							}
+						}
+
+						em.getTransaction().commit();
+					} catch(Exception ex) {
+						ex.printStackTrace();
+						if (em.getTransaction().isActive())
+							em.getTransaction().rollback();
+						continue;
+					}
 				}
-			} catch(Exception ex) {
-				ex.printStackTrace();
-				if (em.getTransaction().isActive())
-					em.getTransaction().rollback();
-				throw ex;
-			} finally {
-				em.clear();
-				if (em.isOpen())
-					em.close();
+
 			}
+		} catch(Exception ex) {
+			ex.printStackTrace();
+			if (em.getTransaction().isActive())
+				em.getTransaction().rollback();
+			throw ex;
+		} finally {
+			em.clear();
+			if (em.isOpen())
+				em.close();
 		}
 	}
 }
