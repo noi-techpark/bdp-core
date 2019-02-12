56e80a231d9d7fd85e01f955c15b12fb2ddc093f
Peter Moser <peter.moser@idm-suedtirol.com>
2019-02-12 10:48:51 +0100

    Add schema generator tools

---
+++ tools/README.md
@@ -0,0 +1,40 @@
+# BIG DATA PLATFORM - Schema Generator
+
+Generate a schema dump of all entities inside a given path and write it into a file.
+
+## Short version
+To generate a new schema, execute the shell script `bdp-sqldump.sh`.
+
+## Detailed version
+
+This chapter is about the Java tool itself, the shell script above simplifies these steps.
+
+The output patterns, that define how identifiers are written, are defined inside the implicit naming strategy class.
+For example, any unique constraint starts with `uc_` and contains the current table and used columns, all lower case
+with underscores.  For more details see code comments.
+
+### Compilation:
+  `mvn clean package`
+
+### Example execution:
+Dump BDP/dal entity classes:
+```
+    java -cp '../dto/target/dto-2.0.0.jar:../dal/target/dal-2.0.0.jar:target/schemagenerator-1.0.0.jar' \
+         -Dlog4j.configuration=src/main/resources/log4j.properties \
+         it.bz.idm.bdp.tools.SchemaGenerator \
+         it.bz.idm.bdp.dal \
+         /tmp/schema_dump1.sql
+```
+
+Hereby, `../dto/target/dto-2.0.0.jar:../dal/target/dal-2.0.0.jar` are class path entries that contain
+the `@Entity` annotated classes, that should be dumped as SQL DDL commands.  `target/schemagenerator-1.0.0.jar`
+is the library that contains the `main` method.  `-Dlog4j.configuration=src/main/resources/log4j.properties` is
+to override log levels, which may be too verbose.  `it.bz.idm.bdp.tools.SchemaGenerator` is the class that
+contains the main method to be executed.  The script scans `it.bz.idm.bdp.dal` for entity classes, and dumps the
+result into `/tmp/schema_dump1.sql`.
+
+### Known issues
+You can provide PostgreSQL connection information. However, the script also works if they are wrong.  That is,
+because of how the registry builder works. The registry builder needs a data source, however, also if the
+connection fails it succeeds in building a DDL SQL script.  The drawback hereby is, that it generates a warning
+on stderr, which cannot be avoided with a try-catch...
---
+++ tools/bdp-sqldump.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+set -euo pipefail
+
+VERSION="1.0.0"
+
+echo
+echo "## BIG DATA PLATFORM -- SQL GENERATOR v$VERSION ##"
+echo 
+echo "NB: This script must be executed from the BDP-CORE root directory!"
+echo
+
+test $# -eq 1 || {
+    echo "USAGE: $0 OUTPUTVERSION"
+    echo "  The OUTPUTVERSION must correspond to DTO and DAL versions (see their pom.xml files)."
+    echo
+    echo "  Example: $0 2.0.0"
+    echo
+    exit 1
+}
+
+# Root directory of your bdp-core source
+BDPROOT=$(pwd)
+
+# Re-assign variables, to be sure they are not "unbound", OUTPUTVERSION must correspond to DAL and DTO versions
+OUTPUTVERSION=$1
+
+# Jar files and classpath
+GEN="$BDPROOT/tools/target/schemagenerator-$VERSION.jar"
+DTO="$BDPROOT/dto/target/dto-$OUTPUTVERSION.jar"
+DAL="$BDPROOT/dal/target/dal-$OUTPUTVERSION.jar"
+CLASSPATH="$DTO:$DAL:$GEN"
+
+# Classes and paths
+GENERATORMAIN='it.bz.idm.bdp.tools.SchemaGenerator'
+CLASSPREFIX="it.bz.idm.bdp.dal"
+STRATEGYCLASS='it.bz.idm.bdp.dal.util.SchemaGeneratorImplicitNamingStrategy'
+
+# Output
+OUTPUTPATH="$BDPROOT/dal/src/main/resources/META-INF/sql"
+OUTPUTFILE="$OUTPUTPATH/schema-$OUTPUTVERSION-dump.sql"
+
+# Build the jar file, if it does not yet exist
+test -f "$GEN" || {
+    cd tools
+    mvn clean package
+    cd -
+    test -f "$GEN" || {
+        echo "ERROR: $GEN not found. Maybe you have a version mismatch with $VERSION. See:"
+        ls $(dirname $GEN)/*.jar
+        exit 1
+    }
+}
+test -f "$DTO" || {
+    cd dto
+    mvn clean package
+    cd -
+    test -f "$DTO" || {
+        echo "ERROR: $DTO not found. Maybe you have a version mismatch with $OUTPUTVERSION. See:"
+        ls $(dirname $DTO)/*.jar
+        exit 1
+    }
+}
+test -f "$DAL" || {
+    cd dal
+    mvn clean package
+    cd -
+    test -f "$DAL" || {
+        echo "ERROR: $DAL not found. Maybe you have a version mismatch with $OUTPUTVERSION. See:"
+        ls $(dirname $DAL)/*.jar
+        exit 1
+    }
+}
+
+
+# Do not overwrite existing files, move them to .backup
+EXISTED=""
+test -f "$OUTPUTFILE" && {
+    mv "$OUTPUTFILE" "$OUTPUTPATH/schema-$OUTPUTVERSION-dump.sql.backup"
+    echo "File '$OUTPUTFILE' exists, we move the original one to $OUTPUTPATH/schema-$OUTPUTVERSION-dump.sql.backup"
+}
+
+# Separate multiple .jar files defined inside parameter $1 with : (colon)
+# We override log4j properties to minimize verbose outputs
+# A known issue throws always exceptions, therefore we pipe it to /dev/null
+cd tools
+temp_file=$(mktemp)
+java -cp "$CLASSPATH" -Dlog4j.configuration=src/main/resources/log4j.properties \
+        $GENERATORMAIN $CLASSPREFIX $STRATEGYCLASS $OUTPUTFILE 2>temp_file || {
+    RES=$?
+    echo "ERROR: Response code:" $RES
+    echo "ERROR: Message:"
+    cat temp_file
+    rm temp_file
+    cd -
+    exit $RES
+}
+
+cd -
+
+echo 
+exit 0
