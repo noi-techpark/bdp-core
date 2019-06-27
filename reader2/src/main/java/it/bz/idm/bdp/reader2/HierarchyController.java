69fcbf6edce153290fdd5297ac90eb7879035d65
Peter Moser <p.moser@noi.bz.it>
2019-06-27 14:21:38 +0200

    reader2: Improve Swagger documentation

--- reader2/src/main/java/it/bz/idm/bdp/reader2/HierarchyController.java
+++ reader2/src/main/java/it/bz/idm/bdp/reader2/HierarchyController.java
@@ -47,12 +47,16 @@ import io.swagger.annotations.ApiParam;
 @Api(value = "Hierarchy", produces = "application/json")
 public class HierarchyController {
 
-	private static final String DOC_STATIONTYPES = "Station types or categories. Multiple types possible as comma-separated-values.";
-	private static final String DOC_DATATYPES = "Data types. Multiple types possible as comma-separated-values.";
+	private static final String DOC_STATIONTYPES = "Station types or categories. Multiple types possible as comma-separated-values. All types with <code>*</code>.";
+	private static final String DOC_DATATYPES = "Data types. Multiple types possible as comma-separated-values. All types with <code>*</code>.";
 	private static final String DOC_LIMIT = "The limit of the response. Set it to -1 to disable it.";
 	private static final String DOC_OFFSET = "The offset of the response list. To simulate pagination, together with limit.";
-	private static final String DOC_SELECT = "Select JSON keys (a.k.a. column-aliases), which will be used to build the response. Multiple aliases possible as comma-separated-values.";
-	private static final String DOC_WHERE = "Filter the result with filter-triples, like <code>alias.operator.value</code>\nOperators can be \n" +
+	private static final String DOC_SELECT = "Select JSON keys (a.k.a. column-aliases), which will be used to build the response. Multiple aliases possible as comma-separated-values. All of them start with a single character prefix, ex., <code>sname</code>.";
+	private static final String DOC_WHERE = "Filter the result with filter-triples, like <code>alias.operator.value_or_list</code>" +
+			"\n\n<code>values_or_list</code>\n" +
+			" -   value: Whatever you want, also a regular expression. However, you need to escape <code>,</code> and <code>'</code> with a <code>\\\\</code>. Use url-encoded values, if your tool does not support certain characters.\n" +
+			" -   list: <code>(value,value,value)</code>" +
+			"\n\n<code>operator</code>\n" +
 			" -   eq: Equal\n" +
 			" -   neq: Not Equal\n" +
 			" -   lt: Less Than\n" +
@@ -66,8 +70,8 @@ public class HierarchyController {
 			" -   bbi: Bounding box intersecting objects (ex., a street that is only partially covered by the box). Syntax? See below.\n" +
 			" -   bbc: Bounding box containing objects (ex., a station or street, that is completely covered by the box). Syntax? See below.\n" +
 			" -   in: True, if the value of the alias can be found within the given list. Example: name.in.(Peter,Patrick,Rudi)\n" +
-			" -   and(alias.operator.value,...): Conjunction of filters (can be nested)\n" +
-			" -   or(alias.operator.value,...): Disjunction of filters (can be nested)\n" +
+			" -   and(alias.operator.value_or_list,...): Conjunction of filters (can be nested)\n" +
+			" -   or(alias.operator.value_or_list,...): Disjunction of filters (can be nested)\n" +
 			"\nMultiple conditions possible as comma-separated-values.\n\n" +
 			" Example-syntax for bbi/bbc could be <code>coordinate.bbi.(11,46,12,47,4326)</code>, where the ordering inside the list is left-x, left-y, right-x, right-y and SRID (optional).";
 	private static final String DOC_SHOWNULL = "Should JSON keys with null-values be returned, or removed from the response-JSON.";
@@ -100,7 +104,7 @@ public class HierarchyController {
 			notes = "You can put multiple station types as comma-seperated list.<br>The response is a hierarchy of <code>station-type / station-name</code>."
 			)
 	@GetMapping(value = "/{stationTypes}", produces = "application/json")
-	public @ResponseBody String requestStations(@ApiParam(value=DOC_STATIONTYPES) @PathVariable String stationTypes,
+	public @ResponseBody String requestStations(@ApiParam(value=DOC_STATIONTYPES, defaultValue="*") @PathVariable String stationTypes,
 											    @ApiParam(value=DOC_LIMIT) @RequestParam(value="limit", required=false, defaultValue=DEFAULT_LIMIT) Long limit,
 											    @ApiParam(value=DOC_OFFSET) @RequestParam(value="offset", required=false, defaultValue=DEFAULT_OFFSET) Long offset,
 											    @ApiParam(value=DOC_SELECT) @RequestParam(value="select", required=false) String select,
@@ -113,8 +117,8 @@ public class HierarchyController {
 			value = "View details of all given station types including data types and most-recent measurements",
 			notes = "You can put multiple station or data types as comma-seperated lists.<br>The response is a hierarchy of <code>station-type / station-name / data-type / measurements</code>.")
 	@GetMapping(value = "/{stationTypes}/{dataTypes}", produces = "application/json")
-	public @ResponseBody String requestDataTypes(@ApiParam(value=DOC_STATIONTYPES) @PathVariable String stationTypes,
-												 @ApiParam(value=DOC_DATATYPES) @PathVariable String dataTypes,
+	public @ResponseBody String requestDataTypes(@ApiParam(value=DOC_STATIONTYPES, defaultValue="*") @PathVariable String stationTypes,
+												 @ApiParam(value=DOC_DATATYPES, defaultValue="*") @PathVariable String dataTypes,
 												 @ApiParam(value=DOC_LIMIT) @RequestParam(value="limit", required=false, defaultValue=DEFAULT_LIMIT) Long limit,
 												 @ApiParam(value=DOC_OFFSET) @RequestParam(value="offset", required=false, defaultValue=DEFAULT_OFFSET) Long offset,
 												 @ApiParam(value=DOC_SELECT) @RequestParam(value="select", required=false) String select,
@@ -127,8 +131,8 @@ public class HierarchyController {
 			value = "View details of all given station types including data types and historical measurements",
 			notes = "You can put multiple station or data types as comma-seperated lists.<br>The response is a hierarchy of <code>station-type / station-name / data-type / measurements</code>.")
 	@GetMapping(value = "/{stationTypes}/{dataTypes}/{from}/{to}", produces = "application/json")
-	public @ResponseBody String requestHistory(@ApiParam(value=DOC_STATIONTYPES) @PathVariable String stationTypes,
-											   @ApiParam(value=DOC_DATATYPES) @PathVariable String dataTypes,
+	public @ResponseBody String requestHistory(@ApiParam(value=DOC_STATIONTYPES, defaultValue="*") @PathVariable String stationTypes,
+											   @ApiParam(value=DOC_DATATYPES, defaultValue="*") @PathVariable String dataTypes,
 											   @ApiParam(value=DOC_TIME) @PathVariable String from,
 											   @ApiParam(value=DOC_TIME) @PathVariable String to,
 											   @ApiParam(value=DOC_LIMIT) @RequestParam(value="limit", required=false, defaultValue=DEFAULT_LIMIT) Long limit,
