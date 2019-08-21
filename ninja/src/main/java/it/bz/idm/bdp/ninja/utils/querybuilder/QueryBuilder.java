03b2f7d99c0e49e34e5d051c3940cb42197fb4de
Peter Moser <p.moser@noi.bz.it>
2019-08-21 14:44:39 +0200

    Ninja: Allow to add SQL snippets before and after a select element during expansion

--- ninja/src/main/java/it/bz/idm/bdp/ninja/utils/querybuilder/QueryBuilder.java
+++ ninja/src/main/java/it/bz/idm/bdp/ninja/utils/querybuilder/QueryBuilder.java
@@ -23,11 +23,16 @@ public class QueryBuilder {
 		if (QueryBuilder.se == null) {
 			throw new RuntimeException("Missing Select Expansion. Run QueryBuilder.setup before initialization.");
 		}
+		reset(select, where, selectDefNames);
+	}
+
+	public QueryBuilder reset(final String select, final String where, String... selectDefNames) {
 		if (select == null || select.isEmpty()) {
 			throw new RuntimeException("No alias list defined. For example: \"name, age, gender\".");
 		}
 		se.setWhereClause(where);
 		se.expand(select, selectDefNames);
+		return this;
 	}
 
 	/**
