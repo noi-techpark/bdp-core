5cb3002dcbfd49885a09800eeee7588b443cf918
bertolla <p.bertolla@noi.bz.it>
2019-09-06 16:42:06 +0200

    define new rules for mobility devs

--- dal/src/main/resources/META-INF/sql/opendatarules.sql
+++ dal/src/main/resources/META-INF/sql/opendatarules.sql
@@ -13,6 +13,11 @@ delete from bdprules ru  where ru.role_id in (select id from bdprole where name=
 insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'CBZ' and (s.stationtype='Mobilestation');
 insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'CBZ' and (s.stationtype='TrafficSensor' and s.origin='FAMAS-traffic');
 
+
+-- define rules for developers working on analytics mobility
+delete from bdprules ru  where ru.role_id in (select id from bdprole where name='MAD');
+insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'MAD' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','EChargingPlug','EChargingStation','EnvironmentStation','Linkstation','MeteoStation','Mobilestation','ParkingStation','RWISstation','Streetstation','traffic','TrafficSensor','Trafficstation','TrafficStreetFactor','VMS');
+
 -- define rules for brennerlec user by making him inherit guest rules and adding specific stations
 update bdprole set parent_id = (select id from bdprole where name='GUEST') where id in (select id from bdprole where name='BLC');
 delete from bdprules ru  where ru.role_id in (select id from bdprole where name='BLC');
@@ -21,5 +26,7 @@ insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdpr
 insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'BLC' and s.origin = 'A22-traffic' and stationtype ='TrafficSensor';
 insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'BLC' and s.origin = 'A22-traffic' and stationtype ='RWISstation';
 insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'BLC' and s.origin = 'A22-traffic' and stationtype ='VMS';
+
+
 refresh materialized view bdppermissions;
-commit;
+commit;s
