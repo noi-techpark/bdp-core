f86e681729e16430abbea1d3077e1796cabe70df
Peter Moser <p.moser@noi.bz.it>
2019-11-28 14:11:08 +0100

    quickrelease.sh: Keep old DTO versions for WS-INTERFACE

--- quickrelease.sh
+++ quickrelease.sh
@@ -12,13 +12,15 @@ test "$TYPE" = "release" -o "$TYPE" = "snapshot" || {
 # UPDATING pom.xml files...
 test "$TYPE" = "snapshot" && VERSION="$VERSION-SNAPSHOT"
 
+### Configuration
 REP="maven-repo.opendatahub.bz.it"
 REP_ID="$REP-$TYPE"
 REP_URL="http://it.bz.opendatahub.s3-website-eu-west-1.amazonaws.com/$TYPE"
 XMLNS=http://maven.apache.org/POM/4.0.0
 CMD="xmlstarlet ed -P -L -N pom=$XMLNS"
+DTO_VERSION_FOR_WS="3.1.0"
 
-for FOLDER in dto dal reader writer dc-interface
+for FOLDER in dto dal reader writer dc-interface ws-interface
 do
     $CMD -u "/pom:project/pom:repositories/pom:repository[starts-with(pom:id,'$REP')]/pom:url" -v $REP_URL $FOLDER/pom.xml
     $CMD -u "/pom:project/pom:repositories/pom:repository[starts-with(pom:id,'$REP')]/pom:id" -v $REP_ID $FOLDER/pom.xml
@@ -27,6 +29,16 @@ do
     $CMD -u "/pom:project/pom:dependencies/pom:dependency[pom:groupId='it.bz.idm.bdp'][pom:artifactId='dto']/pom:version" -v $VERSION $FOLDER/pom.xml
     $CMD -u "/pom:project/pom:dependencies/pom:dependency[pom:groupId='it.bz.idm.bdp'][pom:artifactId='dal']/pom:version" -v $VERSION $FOLDER/pom.xml
 done
+
+# WARNING: ws-interface must always use a fixed DTO package version (DTO_VERSION_FOR_WS)
+#
+# The newer dto versions are just for the dc-interface and writer. This is
+# because the metadata gets flattened and injected directly at level 1 of the
+# JSON hierarchy. However, this could overwrite existing json attributes, which
+# happens with data collectors sometimes. The solution is now to keep an old
+# version for webservices, where the API is stable and just update it for data
+# collectors, which can be adapted easily.
+$CMD -u "/pom:project/pom:dependencies/pom:dependency[pom:groupId='it.bz.idm.bdp'][pom:artifactId='dto']/pom:version" -v $DTO_VERSION_FOR_WS $FOLDER/pom.xml
 
 # UPDATING CONTRIBUTORS.rst...
 bash CONTRIBUTORS.rst
