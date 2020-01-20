373c5acad9aa4900186edad47a8615fc7d48b321
bertolla <p.bertolla@noi.bz.it>
2020-01-20 10:46:00 +0100

    switch link station to correct naming convention

--- dal/src/main/resources/META-INF/sql/opendatarules.sql
+++ dal/src/main/resources/META-INF/sql/opendatarules.sql
@@ -26,7 +26,7 @@ begin;
 -- EChargingStation    | route220
 -- EnvironmentStation  | APPATN-open
 -- EnvironmentStation  | APPABZ | 3600
--- Linkstation         | 
+-- LinkStation         | 
 -- MeteoStation        | meteotrentino
 -- MeteoStation        | SIAG
 -- Mobilestation       | 
@@ -36,7 +36,7 @@ begin;
 -- RWISstation         | InfoMobility
 -- Streetstation       | 
 delete from bdprules ru  where ru.role_id in (select id from bdprole where name='GUEST');
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'GUEST' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','EChargingPlug','EChargingStation','EnvironmentStation', 'Linkstation','MeteoStation','Mobilestation','ParkingStation','RWISstation','Streetstation') and (s.origin is null or s.origin in('ALGORAB','FLOOTA','HAL-API','CARSHARINGBZ','DRIWE','ALPERIA','IIT','route220','Nevicam','APPATN-open','meteotrentino','SIAG','FAMAS','Municipality Merano','FBK','InfoMobility','APPABZ'));
+insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'GUEST' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','EChargingPlug','EChargingStation','EnvironmentStation', 'LinkStation','MeteoStation','Mobilestation','ParkingStation','RWISstation','Streetstation') and (s.origin is null or s.origin in('ALGORAB','FLOOTA','HAL-API','CARSHARINGBZ','DRIWE','ALPERIA','IIT','route220','Nevicam','APPATN-open','meteotrentino','SIAG','FAMAS','Municipality Merano','FBK','InfoMobility','APPABZ'));
 
 update bdprules set period = 3600 where station_id in (select id from station where origin = 'APPABZ') and role_id=(select id from bdprole where name='GUEST');
 
@@ -49,7 +49,7 @@ insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdpr
 
 -- define rules for developers working on analytics mobility
 delete from bdprules ru  where ru.role_id in (select id from bdprole where name='MAD');
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'MAD' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','EChargingPlug','EChargingStation','EnvironmentStation','Linkstation','MeteoStation','Mobilestation','ParkingStation','RWISstation','Streetstation','traffic','TrafficSensor','Trafficstation','TrafficStreetFactor','VMS');
+insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'MAD' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','EChargingPlug','EChargingStation','EnvironmentStation','LinkStation','MeteoStation','Mobilestation','ParkingStation','RWISstation','Streetstation','traffic','TrafficSensor','Trafficstation','TrafficStreetFactor','VMS');
 
 -- define rules for brennerlec user by making him inherit guest rules and adding specific stations
 update bdprole set parent_id = (select id from bdprole where name='GUEST') where id in (select id from bdprole where name='BLC');
