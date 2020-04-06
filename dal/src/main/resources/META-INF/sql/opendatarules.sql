f00939a8818868a272843a7a2cb0557a23f6ad24
PaddyBee <patrick.bertolla@gmail.com>
2020-04-06 16:20:55 +0200

    add opendatarules

--- dal/src/main/resources/META-INF/sql/opendatarules.sql
+++ dal/src/main/resources/META-INF/sql/opendatarules.sql
@@ -4,40 +4,55 @@ begin;
 --     stationtype     |                    origin       | period             
 ---------------------+----------------------------------------------
 -- Bicycle             | ALGORAB
+-- Bicycle             | BIKE_SHARING_MERANO
 -- BikesharingStation  | ALGORAB
--- BluetoothStation    | 
+-- BluetoothStation    |
 -- CarpoolingHub       | FLOOTA
 -- CarpoolingService   | FLOOTA
 -- CarpoolingUser      | FLOOTA
 -- CarsharingCar       | HAL-API
 -- CarsharingStation   | CARSHARINGBZ
 -- CarsharingStation   | HAL-API
--- EChargingPlug       | DRIWE
--- EChargingPlug       | ALPERIA
--- EChargingPlug       | 
--- EChargingPlug       | IIT
--- EChargingPlug       | route220
+-- Covid-19            | provincebz
+-- CreativeIndustry    | 1I5Zj7JHprwLhzl9ktXJO-7v7x3rh1ysGbiblZgZYCXU
 -- EChargingPlug       | Nevicam
--- EChargingStation    | Nevicam
--- EChargingStation    | 
+-- EChargingPlug       | DRIWE
+-- EChargingPlug       | route220
+-- EChargingPlug       | ALPERIA
+-- EChargingPlug       |
+-- EChargingPlug       | IIT
 -- EChargingStation    | IIT
+-- EChargingStation    | Nevicam
+-- EChargingStation    |
+-- EChargingStation    | route220
 -- EChargingStation    | ALPERIA
 -- EChargingStation    | DRIWE
--- EChargingStation    | route220
+-- EnvironmentStation  | APPABZ
+-- EnvironmentStation  | FAMAS-traffic
 -- EnvironmentStation  | APPATN-open
--- EnvironmentStation  | APPABZ | 3600
--- LinkStation         | 
--- MeteoStation        | meteotrentino
+-- EnvironmentStation  | APPATN
+-- LinkStation         |
 -- MeteoStation        | SIAG
--- Mobilestation       | 
+-- MeteoStation        | FAMAS-traffic
+-- MeteoStation        | meteotrentino
+-- Mobilestation       |
+-- NOI-Place           | 1SSXusoMlNpQd-_CtKjft2Zh2yaWhoqNes4GzZl_X0GI
+-- ParkingSensor       | AXIANS
 -- ParkingStation      | FAMAS
 -- ParkingStation      | Municipality Merano
 -- ParkingStation      | FBK
 -- RWISstation         | InfoMobility
--- Streetstation       | 
+-- Streetstation       |
+-- traffic             |
+-- TrafficSensor       | FAMAS-traffic
+-- TrafficSensor       | A22
+-- Trafficstation      |
+-- TrafficStreetFactor |
+-- VMS                 | A22
+
 delete from bdprules ru  where ru.role_id in (select id from bdprole where name='GUEST');
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'GUEST' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','EChargingPlug','EChargingStation','EnvironmentStation', 'LinkStation','MeteoStation','Mobilestation','ParkingStation','RWISstation','Streetstation') and (s.origin is null or s.origin in('ALGORAB','FLOOTA','HAL-API','CARSHARINGBZ','DRIWE','ALPERIA','IIT','route220','Nevicam','APPATN-open','meteotrentino','SIAG','FAMAS','Municipality Merano','FBK','InfoMobility','APPABZ'));
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'GUEST' and s.stationtype in ('NOI-Place');
+insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'GUEST' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','EChargingPlug','EChargingStation','EnvironmentStation', 'LinkStation','MeteoStation','Mobilestation','ParkingSensor','ParkingStation','RWISstation','Streetstation') and (s.origin is null or s.origin in('ALGORAB','FLOOTA','HAL-API','CARSHARINGBZ','DRIWE','ALPERIA','IIT','route220','Nevicam','APPATN-open','meteotrentino','SIAG','FAMAS','Municipality Merano','FBK','InfoMobility','APPABZ','BIKE_SHARING_MERANO','AXIANS'));
+insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'GUEST' and s.stationtype in ('NOI-Place','CreativeIndustry');
 update bdprules set period = 3600 where station_id in (select id from station where origin = 'APPABZ') and role_id=(select id from bdprole where name='GUEST');
 
 -- define rules for cbz user by making him inherit guest rules and adding specific stations
@@ -49,7 +64,7 @@ insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdpr
 
 -- define rules for developers working on analytics mobility
 delete from bdprules ru  where ru.role_id in (select id from bdprole where name='MAD');
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'MAD' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','EChargingPlug','EChargingStation','EnvironmentStation','LinkStation','MeteoStation','Mobilestation','ParkingStation','RWISstation','Streetstation','traffic','TrafficSensor','Trafficstation','TrafficStreetFactor','VMS');
+insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'MAD' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','Covid-19','EChargingPlug','EChargingStation','EnvironmentStation','LinkStation','MeteoStation','Mobilestation','ParkingStation','RWISstation','Streetstation','traffic','TrafficSensor','Trafficstation','TrafficStreetFactor','VMS');
 
 -- define rules for brennerlec user by making him inherit guest rules and adding specific stations
 update bdprole set parent_id = (select id from bdprole where name='GUEST') where id in (select id from bdprole where name='BLC');
