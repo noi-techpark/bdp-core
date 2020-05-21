bb962838dec12e5fbb362abfbb43c8cb097c81cd
Martin Malfertheiner <martin.malfertheiner@gmail.com>
2020-05-21 16:59:17 +0200

    setup deployment of ninja with docker

--- Jenkinsfile-Ninja-Test.groovy
+++ Jenkinsfile-Ninja-Test.groovy
@@ -1,64 +1,77 @@
 pipeline {
     agent {
-        dockerfile {
-            filename 'docker/dockerfile-java'
-            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
-        }
+        any
     }
 
     environment {
-        TESTSERVER_TOMCAT_ENDPOINT = credentials('testserver-tomcat8-url')
-        TESTSERVER_TOMCAT_CREDENTIALS = credentials('testserver-tomcat8-credentials')
+        DOCKER_PROJECT_NAME = "ninja"
+        DOCKER_IMAGE = '755952719952.dkr.ecr.eu-west-1.amazonaws.com/ninja'
+        DOCKER_TAG = "test-$BUILD_NUMBER"
 
-        NINJA_ROOT_PATH = "ninja"
-        NINJA_LOGGING_FILE = "/var/log/opendatahub/ninja.log"
-        NINJA_KEYCLOAK_SERVERURL = "https://auth.opendatahub.testingmachine.eu/auth/"
+        SERVER_PORT = "1002"
+        NINJA_HOST_URL = "https://ninja.testingmachine.eu"
+        NINJA_BASE_URL = "${NINJA_HOST_URL}/v2"
 
-        NINJA_DATABASE_SCHEMA = "intimev2"
-        NINJA_DATABASE_HOST = "test-pg-bdp.co90ybcr8iim.eu-west-1.rds.amazonaws.com"
-        NINJA_DATABASE_PORT = "5432"
-        NINJA_DATABASE_NAME = "bdp"
-        NINJA_DATABASE_READ_USER = "bdp_readonly"
-        NINJA_DATABASE_READ_PASSWORD = credentials('bdp-core-test-database-read-password')
+        SECURITY_ALLOWED_ORIGINS = "*"
+        KEYCLOAK_URL = "https://auth.opendatahub.testingmachine.eu/auth/"
+        KEYCLOAK_SSL_REQUIRED = "none"
+        KEYCLOAK_REALM = "noi"
+        KEYCLOAK_CLIENT_ID = "odh-mobility-v2"
+        KEYCLOAK_CLIENT_SECRET = 
 
-        NINJA_HOST_URL = "https://tomcatsec.testingmachine.eu"
-		NINJA_BASE_URL = "${NINJA_HOST_URL}/v2"
+        JDBC_URL = "jdbc:postgresql://test-pg-bdp.co90ybcr8iim.eu-west-1.rds.amazonaws.com:5432/bdp?currentSchema=intimev2,public"
+        DB_USERNAME = "bdp_readonly"
+        DB_PASSWORD = credentials('bdp-core-test-database-read-password')
     }
 
     stages {
         stage('Configure') {
             steps {
                 sh '''
-                    sed -i -e "s/<\\/settings>$//g\" ~/.m2/settings.xml
-                    echo "    <servers>" >> ~/.m2/settings.xml
-                    echo "        ${TESTSERVER_TOMCAT_CREDENTIALS}" >> ~/.m2/settings.xml
-                    echo "    </servers>" >> ~/.m2/settings.xml
-                    echo "</settings>" >> ~/.m2/settings.xml
-
-                    cp "${NINJA_ROOT_PATH}/src/main/resources/application.properties.dist" "${NINJA_ROOT_PATH}/src/main/resources/application.properties"
-                    sed -i -e "s%\\(logging.file\\s*=\\).*\\$%\\1${NINJA_LOGGING_FILE}%" ${NINJA_ROOT_PATH}/src/main/resources/application.properties
-                    sed -i -e "s%\\(logging.level.root\\s*=\\).*\\$%\\1DEBUG%" ${NINJA_ROOT_PATH}/src/main/resources/application.properties
-                    sed -i -e "s%\\(logging.level.org.springframework.jdbc.core\\s*=\\).*\\$%\\1TRACE%" ${NINJA_ROOT_PATH}/src/main/resources/application.properties
-                    sed -i -e "s%\\(keycloak.auth-server-url\\s*=\\).*\\$%\\1 ${NINJA_KEYCLOAK_SERVERURL}%" ${NINJA_ROOT_PATH}/src/main/resources/application.properties
-
-                    sed -i -e "s%__ODH_SERVER_URL__%${NINJA_HOST_URL}%" ${NINJA_ROOT_PATH}/src/main/resources/openapi3.yml
-                    sed -i -e "s%\\(ninja.url\\s*=\\).*\\$%\\1 ${NINJA_BASE_URL}%" ${NINJA_ROOT_PATH}/src/main/resources/application.properties
-
-                    cp "${NINJA_ROOT_PATH}/src/main/resources/database.properties.dist" "${NINJA_ROOT_PATH}/src/main/resources/database.properties"
-                    sed -i -e "s%\\(username\\s*=\\).*\\$%\\1 ${NINJA_DATABASE_READ_USER}%" ${NINJA_ROOT_PATH}/src/main/resources/database.properties
-                    sed -i -e "s%\\(password\\s*=\\).*\\$%\\1 ${NINJA_DATABASE_READ_PASSWORD}%" ${NINJA_ROOT_PATH}/src/main/resources/database.properties
-                    sed -i -e "s%\\(jdbcUrl\\s*=\\).*\\$%\\1 jdbc:postgresql://${NINJA_DATABASE_HOST}:${NINJA_DATABASE_PORT}/${NINJA_DATABASE_NAME}?currentSchema=${NINJA_DATABASE_SCHEMA},public%" ${NINJA_ROOT_PATH}/src/main/resources/database.properties
+                    rm -f .env
+                    cp .env.example .env
+                    echo 'COMPOSE_PROJECT_NAME=${DOCKER_PROJECT_NAME}' >> .env
+                    echo 'DOCKER_IMAGE=${DOCKER_IMAGE}' >> .env
+                    echo 'DOCKER_TAG=${DOCKER_TAG}' >> .env
+                    echo 'SERVER_PORT=${SERVER_PORT}' >> .env
+                    echo 'NINJA_BASE_URL=${NINJA_BASE_URL}' >> .env
+                    echo 'SECURITY_ALLOWED_ORIGINS=${SECURITY_ALLOWED_ORIGINS}' >> .env
+                    echo 'KEYCLOAK_URL=${KEYCLOAK_URL}' >> .env
+                    echo 'KEYCLOAK_SSL_REQUIRED=${KEYCLOAK_SSL_REQUIRED}' >> .env
+                    echo 'KEYCLOAK_REALM=${KEYCLOAK_REALM}' >> .env
+                    echo 'KEYCLOAK_CLIENT_ID=${KEYCLOAK_CLIENT_ID}' >> .env
+                    echo 'KEYCLOAK_CLIENT_SECRET=${KEYCLOAK_CLIENT_SECRET}' >> .env
+                    echo 'JDBC_URL=${JDBC_URL}' >> .env
+                    echo 'DB_USERNAME=${DB_USERNAME}' >> .env
+                    echo 'DB_PASSWORD=${DB_PASSWORD}' >> .env                    
+                '''
+            }
+        }
+        stage('Test - Ninja') {
+            steps {
+                sh '''
+                    docker-compose --no-ansi build --pull --build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)
+                    docker-compose --no-ansi run --rm --no-deps -u $(id -u jenkins):$(id -g jenkins) app mvn clean test
                 '''
             }
         }
         stage('Build - Ninja') {
             steps {
-                sh 'cd ${NINJA_ROOT_PATH} && mvn -B -U clean test package'
+                sh '''
+                    aws ecr get-login --region eu-west-1 --no-include-email | bash
+                    docker-compose --no-ansi -f docker-compose.build.yml build --pull
+                    docker-compose --no-ansi -f docker-compose.build.yml push
+                '''
             }
         }
         stage('Deploy - Ninja API (v2)') {
             steps {
-                sh 'cd ${NINJA_ROOT_PATH} && mvn -B -DskipTests tomcat:redeploy -Dmaven.tomcat.url=${TESTSERVER_TOMCAT_ENDPOINT} -Dmaven.tomcat.server=testServer -Dmaven.tomcat.path=/v2'
+               sshagent(['jenkins-ssh-key']) {
+                    sh """
+                        ansible-galaxy install --force -r ansible/requirements.yml
+                        ansible-playbook --limit=test ansible/deploy.yml --extra-vars "build_number=${BUILD_NUMBER}"
+                    """
+                }
             }
         }
     }
