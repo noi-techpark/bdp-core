1541250bea05d126cda66e76a7b512c4967e0bf3
Peter Moser <p.moser@noi.bz.it>
2020-05-22 11:34:23 +0200

    CD: Use maven in a non-interactive batch mode

--- Jenkinsfile-Ninja-Test.groovy
+++ Jenkinsfile-Ninja-Test.groovy
@@ -51,7 +51,7 @@ pipeline {
                 sh '''
                     cd ninja
                     docker-compose --no-ansi build --pull --build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)
-                    docker-compose --no-ansi run --rm --no-deps -u $(id -u jenkins):$(id -g jenkins) app mvn clean test
+                    docker-compose --no-ansi run --rm --no-deps -u $(id -u jenkins):$(id -g jenkins) app mvn -B -U clean test
                 '''
             }
         }
