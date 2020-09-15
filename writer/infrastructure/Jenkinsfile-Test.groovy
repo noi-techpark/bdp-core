f53443830b222f7cf4f2495de379a660e770e708
bertolla <p.bertolla@noi.bz.it>
2020-09-15 15:51:05 +0200

    start docker-compose from root folder

--- writer/infrastructure/Jenkinsfile-Test.groovy
+++ writer/infrastructure/Jenkinsfile-Test.groovy
@@ -44,10 +44,9 @@ pipeline {
         stage('Test & Build') {
             steps {
                 sh """
-                    cd ${PROJECT_FOLDER}
                     aws ecr get-login --region eu-west-1 --no-include-email | bash
-                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml build --pull
-                    docker-compose --no-ansi -f infrastructure/docker-compose.build.yml push
+                    docker-compose --no-ansi -f ${PROJECT_FOLDER}/infrastructure/docker-compose.build.yml build --pull
+                    docker-compose --no-ansi -f ${PROJECT_FOLDER}/infrastructure/docker-compose.build.yml push
                 """
             }
         }
