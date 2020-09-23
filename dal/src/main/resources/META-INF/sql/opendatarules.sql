4f5c83d2b2fcac1e01091fba05303a0d8d251929
Peter Moser <p.moser@noi.bz.it>
2020-09-23 08:43:25 +0200

    Update closed and open data rules

--- dal/src/main/resources/META-INF/sql/opendatarules.sql
+++ dal/src/main/resources/META-INF/sql/opendatarules.sql
@@ -50,30 +50,30 @@ begin;
 -- TrafficStreetFactor |
 -- VMS                 | A22
 
-delete from bdprules ru  where ru.role_id in (select id from bdprole where name='GUEST');
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'GUEST' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','EChargingPlug','EChargingStation','EnvironmentStation', 'LinkStation','MeteoStation','Mobilestation','ParkingSensor','ParkingStation','RWISstation','Streetstation') and (s.origin is null or s.origin in('ALGORAB','FLOOTA','HAL-API','CARSHARINGBZ','DRIWE','ALPERIA','IIT','route220','Nevicam','APPATN-open','meteotrentino','SIAG','FAMAS','Municipality Merano','FBK','InfoMobility','APPABZ','BIKE_SHARING_MERANO','AXIANS'));
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'GUEST' and s.stationtype in ('NOI-Place','CreativeIndustry');
-update bdprules set period = 3600 where station_id in (select id from station where origin = 'APPABZ') and role_id=(select id from bdprole where name='GUEST');
+delete from bdprules ru where ru.role_id in (select id from bdprole where name = 'GUEST');
+insert into bdprules (role_id, station_id) select r.id, s.id from station s, bdprole r where r.name = 'GUEST' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','EChargingPlug','EChargingStation','EnvironmentStation', 'LinkStation','MeteoStation','Mobilestation','ParkingSensor','ParkingStation','RWISstation','Streetstation') and (s.origin is null or s.origin in('ALGORAB','FLOOTA','HAL-API','CARSHARINGBZ','DRIWE','ALPERIA','IIT','route220','Nevicam','APPATN-open','meteotrentino','SIAG','FAMAS','Municipality Merano','FBK','InfoMobility','BIKE_SHARING_MERANO','AXIANS'));
+insert into bdprules (role_id, station_id) select r.id, s.id from station s, bdprole r where r.name = 'GUEST' and s.stationtype in ('NOI-Place','CreativeIndustry');
+insert into bdprules (role_id, station_id) select r.id, s.id from station s, bdprole r where r.name = 'GUEST' and s.stationtype = 'LinkStation' and s.origin = 'NOI';
+insert into bdprules (role_id, station_id, period) select r.id, s.id, 3600 from station s, bdprole r where r.name = 'GUEST' and s.origin = 'APPABZ';
 
 -- define rules for cbz user by making him inherit guest rules and adding specific stations
-update bdprole set parent_id = (select id from bdprole where name='GUEST') where id in (select id from bdprole where name='CBZ');
-delete from bdprules ru  where ru.role_id in (select id from bdprole where name='CBZ');
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'CBZ' and (s.stationtype='Mobilestation');
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'CBZ' and (s.stationtype='TrafficSensor' and s.origin='FAMAS-traffic');
-
+update bdprole set parent_id = (select id from bdprole where name = 'GUEST') where id in (select id from bdprole where name = 'CBZ');
+delete from bdprules ru where ru.role_id in (select id from bdprole where name = 'CBZ');
+insert into bdprules (role_id, station_id) select r.id, s.id from station s, bdprole r where r.name = 'CBZ' and s.stationtype = 'Mobilestation';
+insert into bdprules (role_id, station_id) select r.id, s.id from station s, bdprole r where r.name = 'CBZ' and s.stationtype = 'TrafficSensor' and s.origin = 'FAMAS-traffic';
 
 -- define rules for developers working on analytics mobility
-delete from bdprules ru  where ru.role_id in (select id from bdprole where name='MAD');
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'MAD' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','Covid-19','EChargingPlug','EChargingStation','EnvironmentStation','LinkStation','MeteoStation','Mobilestation','ParkingStation','RWISstation','Streetstation','traffic','TrafficSensor','Trafficstation','TrafficStreetFactor','VMS');
+update bdprole set parent_id = (select id from bdprole where name = 'GUEST') where id in (select id from bdprole where name = 'MAD');
+delete from bdprules ru where ru.role_id in (select id from bdprole where name = 'MAD');
+insert into bdprules (role_id, station_id) select r.id, s.id from station s, bdprole r where r.name = 'MAD' and s.stationtype in ('Bicycle','BikesharingStation','BluetoothStation','CarpoolingHub','CarpoolingService','CarpoolingUser','CarsharingCar','CarsharingStation','Covid-19','EChargingPlug','EChargingStation','EnvironmentStation','LinkStation','MeteoStation','Mobilestation','ParkingStation','RWISstation','Streetstation','traffic','TrafficSensor','Trafficstation','TrafficStreetFactor','VMS');
 
 -- define rules for brennerlec user by making him inherit guest rules and adding specific stations
-update bdprole set parent_id = (select id from bdprole where name='GUEST') where id in (select id from bdprole where name='BLC');
-delete from bdprules ru  where ru.role_id in (select id from bdprole where name='BLC');
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'BLC' and s.stationcode = 'APPATN_27';
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'BLC' and s.origin in ('APPABZ','A22-algorab');
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'BLC' and s.origin = 'A22-traffic' and stationtype ='TrafficSensor';
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'BLC' and s.origin = 'A22-traffic' and stationtype ='RWISstation';
-insert into bdprules (role_id,station_id) select r.id,s.id from  station s, bdprole r where r.name = 'BLC' and s.origin = 'A22-traffic' and stationtype ='VMS';
+update bdprole set parent_id = (select id from bdprole where name = 'GUEST') where id in (select id from bdprole where name = 'BLC');
+delete from bdprules ru where ru.role_id in (select id from bdprole where name = 'BLC');
+insert into bdprules (role_id, station_id) select r.id, s.id from station s, bdprole r where r.name = 'BLC' and s.origin = 'A22';
+insert into bdprules (role_id, station_id) select r.id, s.id from station s, bdprole r where r.name = 'BLC' and s.origin = 'a22-algorab';
+insert into bdprules (role_id, station_id) select r.id, s.id from station s, bdprole r where r.name = 'BLC' and s.origin = 'APPABZ';
+insert into bdprules (role_id, station_id) select r.id, s.id from station s, bdprole r where r.name = 'BLC' and s.origin = 'APPATN';
 
 
 refresh materialized view bdppermissions;
