e71b6876d4c5092473ede464a8245781814dde64
Peter Moser <p.moser@noi.bz.it>
2021-12-06 16:14:26 +0100

    Update utility files

--- quickdocument.sh
+++ quickdocument.sh
@@ -6,14 +6,11 @@ set -xeuo pipefail
 
 TMP=$(mktemp -d /tmp/bdpcoredoc.XXXXXX)
 
-wkhtmltopdf dto/target/site/dependencies.html $TMP/dto-dependencies.pdf
-wkhtmltopdf dal/target/site/dependencies.html $TMP/dal-dependencies.pdf
-wkhtmltopdf dc-interface/target/site/dependencies.html $TMP/dc-interface-dependencies.pdf
-wkhtmltopdf ws-interface/target/site/dependencies.html $TMP/ws-interface-dependencies.pdf
-wkhtmltopdf writer/target/site/dependencies.html $TMP/writer-dependencies.pdf
-wkhtmltopdf reader/target/site/dependencies.html $TMP/reader-dependencies.pdf
+wkhtmltopdf dto/target/site/dependencies.html "$TMP/dto-dependencies.pdf"
+wkhtmltopdf dc-interface/target/site/dependencies.html "$TMP/dc-interface-dependencies.pdf"
+wkhtmltopdf writer/target/site/dependencies.html "$TMP/writer-dependencies.pdf"
 
-pdfunite $TMP/*.pdf dependencies.pdf
+pdfunite "$TMP"/*.pdf dependencies.pdf
 
 echo RESULT: See dependencies.pdf...
 exit 0
