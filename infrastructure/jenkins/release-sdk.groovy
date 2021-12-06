06c3325a95a4580c922277d1d563b9c74c66d9dc
Peter Moser <p.moser@noi.bz.it>
2021-12-06 18:39:06 +0100

    infra: sdk reuse maven downloads

--- infrastructure/jenkins/release-sdk.groovy
+++ infrastructure/jenkins/release-sdk.groovy
@@ -2,12 +2,16 @@ pipeline {
     agent {
         dockerfile {
             filename 'infrastructure/docker/Dockerfile'
-            additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins) --target base'
         }
     }
 
     parameters {
-        string(name: 'TAG', defaultValue: '1.0.0', description: 'Tag pushed to Git. We add the postfix "-SNAPSHOT" for you if you choose the development branch, so write only a plain version')
+        string(
+            name: 'TAG',
+            defaultValue: '1.0.0',
+            description: 'Tag pushed to Git. We add the postfix "-SNAPSHOT" for you if you choose the development branch, so write only a plain version'
+        )
         gitParameter name: 'BRANCH', branchFilter: 'origin/(.*)', defaultValue: 'development', type: 'PT_BRANCH', description: 'Choose either master or development as branch. The tag and version bump commit will be made on it. If you want to make a RELEASE use the master branch, if you want to make a SNAPSHOT release use development.'
     }
 
@@ -28,15 +32,15 @@ pipeline {
         }
         stage('Configure') {
             steps {
-                sh 'sed -i -e "s/<\\/settings>$//g\" ~/.m2/settings.xml'
-                sh 'echo "    <servers>" >> ~/.m2/settings.xml'
-                sh 'echo "        <server>" >> ~/.m2/settings.xml'
-                sh 'echo "            <id>${S3_REPO_ID}</id>" >> ~/.m2/settings.xml'
-                sh 'echo "            <username>${AWS_ACCESS_KEY}</username>" >> ~/.m2/settings.xml'
-                sh 'echo "            <password>${AWS_SECRET_KEY}</password>" >> ~/.m2/settings.xml'
-                sh 'echo "        </server>" >> ~/.m2/settings.xml'
-                sh 'echo "    </servers>" >> ~/.m2/settings.xml'
-                sh 'echo "</settings>" >> ~/.m2/settings.xml'
+                sh 'sed -i -e "s/<\\/settings>$//g\" $WORKSPACE/.m2/settings.xml'
+                sh 'echo "    <servers>" >> $WORKSPACE/.m2/settings.xml'
+                sh 'echo "        <server>" >> $WORKSPACE/.m2/settings.xml'
+                sh 'echo "            <id>${S3_REPO_ID}</id>" >> $WORKSPACE/.m2/settings.xml'
+                sh 'echo "            <username>${AWS_ACCESS_KEY}</username>" >> $WORKSPACE/.m2/settings.xml'
+                sh 'echo "            <password>${AWS_SECRET_KEY}</password>" >> $WORKSPACE/.m2/settings.xml'
+                sh 'echo "        </server>" >> $WORKSPACE/.m2/settings.xml'
+                sh 'echo "    </servers>" >> $WORKSPACE/.m2/settings.xml'
+                sh 'echo "</settings>" >> $WORKSPACE/.m2/settings.xml'
             }
         }
         stage('Preparation for release') {
@@ -44,14 +48,20 @@ pipeline {
                 sh "./infrastructure/utils/quickrelease.sh $REL_TYPE '${params.TAG}'"
             }
         }
-        stage('Deploy dto') {
+        stage('Deploy DTO') {
             steps {
-		        sh 'cd dto/ && mvn -B -U clean test deploy'
+                sh '''
+                    cd dto
+                    mvn -B -U -Duser.home=$WORKSPACE clean test install deploy
+                '''
             }
         }
-        stage('Deploy dc-interface') {
+        stage('Deploy DC-INTERFACE') {
             steps {
-		        sh 'cd dc-interface/ && mvn -B -U clean test deploy'
+                sh '''
+                    cd dc-interface
+                    mvn -B -U -Duser.home=$WORKSPACE clean test deploy
+                '''
             }
         }
         stage('Tag') {
