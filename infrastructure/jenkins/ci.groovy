a5904eec427ebac6e3d7edf4913f71f1826a63cb
Peter Moser <p.moser@noi.bz.it>
2021-12-06 17:52:35 +0100

    ci: build DTO on each step

--- infrastructure/jenkins/ci.groovy
+++ infrastructure/jenkins/ci.groovy
@@ -2,24 +2,30 @@ pipeline {
     agent {
         dockerfile {
             filename 'infrastructure/docker/Dockerfile'
-            additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins) --target ci'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins) --target base'
         }
     }
 
     stages {
         stage('Test - DTO') {
             steps {
-                sh 'cd dto && mvn -B -U clean test install'
+                sh 'cd dto && mvn -B -U clean test'
             }
         }
         stage('Test - DC-INTERFACE') {
             steps {
-                sh 'cd dc-interface && mvn -B -U clean test'
+                sh '''
+                    (cd dto && mvn -B -U -DskipTests install)
+                    (cd dc-interface && mvn -B -U clean test)
+                '''
             }
         }
         stage('Test - Writer') {
             steps {
-                sh 'cd writer && mvn -B -U clean test'
+                sh '''
+                    (cd dto && mvn -B -U -DskipTests install)
+                    (cd writer && mvn -B -U clean test)
+                '''
             }
         }
     }
