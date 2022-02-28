0563d49eeb4fb5662bdacc6a5785e7425cda0d31
Peter Moser <p.moser@noi.bz.it>
2022-02-28 17:24:14 +0100

    Rename PostgisJsonDialect to no longer look at versions

--- writer/src/main/java/it/bz/idm/bdp/dal/util/PostgisJsonDialect.java
+++ writer/src/main/java/it/bz/idm/bdp/dal/util/PostgisJsonDialect.java
@@ -5,9 +5,9 @@ import java.sql.Types;
 import com.vladmihalcea.hibernate.type.json.JsonNodeBinaryType;
 import org.hibernate.spatial.dialect.postgis.PostgisPG10Dialect;
 
-public class PostgisPG10JsonDialect extends PostgisPG10Dialect {
+public class PostgisJsonDialect extends PostgisPG10Dialect {
 
-	public PostgisPG10JsonDialect() {
+	public PostgisJsonDialect() {
 		super();
 		this.registerHibernateType(
 			Types.OTHER, JsonNodeBinaryType.class.getName()
