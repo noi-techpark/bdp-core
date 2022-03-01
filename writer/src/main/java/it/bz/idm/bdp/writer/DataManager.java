39a294818fac6e2e9a13f4ddaaf32e8f492019e4
Peter Moser <p.moser@noi.bz.it>
2022-03-01 13:57:34 +0100

    writer: add persistence configs for flyway

--- writer/src/main/java/it/bz/idm/bdp/writer/DataManager.java
+++ writer/src/main/java/it/bz/idm/bdp/writer/DataManager.java
@@ -33,14 +33,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import it.bz.idm.bdp.dal.DataType;
 import it.bz.idm.bdp.dal.Event;
