a97c2568ac5209a4753feaeb3fdc5a8d7fa8b958
Clemens Zagler <c.zagler@noi.bz.it>
2023-06-05 13:23:50 +0200

    Restore db migration files, because flyway does not allow for modification (REUSE header was added)

--- writer/src/main/resources/db/migration/V02__measurementstring_text.sql
+++ writer/src/main/resources/db/migration/V02__measurementstring_text.sql
@@ -1,7 +1,3 @@
--- SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
---
--- SPDX-License-Identifier: AGPL-3.0-or-later
-
 /*
  * Do not use varchar(n) which just causes issues and is slower on inserts than text
  * See http://www.postgresql.org/docs/current/static/datatype-character.html
--- writer/src/main/resources/db/migration/V03__metadata_history_index.sql
+++ writer/src/main/resources/db/migration/V03__metadata_history_index.sql
@@ -2,10 +2,6 @@
 --
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
-/*
- * Do not use varchar(n) which just causes issues and is slower on inserts than text
- * See http://www.postgresql.org/docs/current/static/datatype-character.html
- */
 set search_path to ${default_schema}, public;
 
 create index IDX_METADATA_HISTORY on metadata(station_id, created_on);
