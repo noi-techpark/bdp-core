ba13726c2ceaf785e32b008b6aa97018154f6219
Clemens Zagler <c.zagler@noi.bz.it>
2024-01-22 13:16:33 +0100

    - Remove _t property from Record DTO. Always use SimpleRecordDTO - Remove deprecated and unused DTO types Text and FullRecord

--- dto/src/main/java/com/opendatahub/timeseries/bdp/dto/dto/RecordDto.java
+++ dto/src/main/java/com/opendatahub/timeseries/bdp/dto/dto/RecordDto.java
@@ -7,14 +7,21 @@ package com.opendatahub.timeseries.bdp.dto.dto;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
 /**
  * describes the validity of a record and makes it serializable
  *
  * @author Patrick Bertolla
  */
-@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="_t")
+@JsonTypeInfo(use = Id.DEDUCTION)
+@JsonSubTypes({@JsonSubTypes.Type(SimpleRecordDto.class)})
+// _t used to be the type information as a class name.
+// now we just ignore it
+@JsonIgnoreProperties(value = {"_t"})
 public interface RecordDto extends Serializable{
 	public abstract boolean validate();
 }
